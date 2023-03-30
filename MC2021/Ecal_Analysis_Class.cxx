9c5e68d4ddbc8036c4c668041970faad69bdb3d6
Maurik Holtrop <maurik@physics.unh.edu>
2023-03-30 16:50:09 -0400

    Get rid of RVec, just use vector.

--- MC2021/Ecal_Analysis_Class.cxx
+++ MC2021/Ecal_Analysis_Class.cxx
@@ -9,25 +9,25 @@ RNode Ecal_Analysis_Class::extend_dataframe(RNode in){
    // To make use of methods in this class that are *not* static, we need to create some lambda function shims to pass
    // the mechod as a funcion to the dataframe.
 
-   auto lambda_get_score_cluster_indexes = [this](RVec<double> mc_score_pz,
-         RVec<double> mc_score_x, RVec<double> mc_score_y, RVec<double> mc_score_z){
+   auto lambda_get_score_cluster_indexes = [this](vector<double> mc_score_pz,
+         vector<double> mc_score_x, vector<double> mc_score_y, vector<double> mc_score_z){
       return get_score_cluster_indexes(mc_score_pz, mc_score_x, mc_score_y, mc_score_z);
    };
 
-   auto lambda_get_score_primary_hits_energy = [this](RVec<double> &mc_part_z, RVec<int> &mc_score_part_idx, RVec<double> &mc_score_z,
-                                                  RVec<double> &mc_score_px, RVec<double> &mc_score_py, RVec<double> &mc_score_pz){
+   auto lambda_get_score_primary_hits_energy = [this](vector<double> &mc_part_z, vector<int> &mc_score_part_idx, vector<double> &mc_score_z,
+                                                  vector<double> &mc_score_px, vector<double> &mc_score_py, vector<double> &mc_score_pz){
       return get_score_primary_hits_energy(mc_part_z, mc_score_part_idx, mc_score_z, mc_score_px, mc_score_py, mc_score_pz);};
 
-   auto lambda_get_score_secondary_hits_energy = [this](RVec<double> &mc_part_z, RVec<int> &mc_score_part_idx, RVec<double> &mc_score_z,
-                                                        RVec<double> &mc_score_px, RVec<double> &mc_score_py, RVec<double> &mc_score_pz){
+   auto lambda_get_score_secondary_hits_energy = [this](vector<double> &mc_part_z, vector<int> &mc_score_part_idx, vector<double> &mc_score_z,
+                                                        vector<double> &mc_score_px, vector<double> &mc_score_py, vector<double> &mc_score_pz){
       return get_score_secondary_hits_energy(mc_part_z, mc_score_part_idx, mc_score_z, mc_score_px, mc_score_py, mc_score_pz);};
 
-   auto lambda_get_score_n_primary_hits = [this](RVec<double> &mc_part_z, RVec<int> &mc_score_part_idx,
-         RVec<double> &mc_score_z, RVec<double> &mc_score_pz){
+   auto lambda_get_score_n_primary_hits = [this](vector<double> &mc_part_z, vector<int> &mc_score_part_idx,
+         vector<double> &mc_score_z, vector<double> &mc_score_pz){
       return get_score_n_primary_hits(mc_part_z, mc_score_part_idx, mc_score_z, mc_score_pz);
    };
-   auto lambda_get_score_n_secondary_hits = [this](RVec<double> &mc_part_z, RVec<int> &mc_score_part_idx,
-         RVec<double> &mc_score_z, RVec<double> &mc_score_pz){
+   auto lambda_get_score_n_secondary_hits = [this](vector<double> &mc_part_z, vector<int> &mc_score_part_idx,
+         vector<double> &mc_score_z, vector<double> &mc_score_pz){
       return get_score_n_secondary_hits(mc_part_z, mc_score_part_idx, mc_score_z, mc_score_pz);
    };
 
@@ -42,16 +42,16 @@ RNode Ecal_Analysis_Class::extend_dataframe(RNode in){
    return ret;
 }
 
-RVec< std::vector<int> > Ecal_Analysis_Class::get_score_cluster_indexes(
-      RVec<double> mc_score_pz,
-      RVec<double> mc_score_x, RVec<double> mc_score_y, RVec<double> mc_score_z){
+vector< vector<int> > Ecal_Analysis_Class::get_score_cluster_indexes(
+      vector<double> mc_score_pz,
+      vector<double> mc_score_x, vector<double> mc_score_y, vector<double> mc_score_z){
 // Return a list of lists of indexes to mc_score array hits.
 // So index=get_score_indexes[cluster_number][hit_in_cluster_number]
 // Conditions for a hit are that the track passed through the score plane at z=1443 in the positive direction with z_momentum > mc_score_pz_cut.
 // Hits are grouped in clusters around the most energetic (in pz) hits if they are within mc_score_close_cut in x and y.
 
-   RVec< std::vector<int> > out;
-   std::vector<bool> has_been_used(mc_score_z.size(), false);
+   vector< vector<int> > out;
+   vector<bool> has_been_used(mc_score_z.size(), false);
    bool not_done = true;
    while(not_done){
       double max_pz = mc_score_pz_cut;
@@ -68,7 +68,7 @@ RVec< std::vector<int> > Ecal_Analysis_Class::get_score_cluster_indexes(
       }else {
          double x_loc = mc_score_x[index_max_pz];
          double y_loc = mc_score_y[index_max_pz];
-         std::vector<int> cluster_list{index_max_pz};
+         vector<int> cluster_list{index_max_pz};
 
          has_been_used[index_max_pz] = true;
          // Second loop to establish the cluster hits beloning to this max_pz
@@ -85,11 +85,11 @@ RVec< std::vector<int> > Ecal_Analysis_Class::get_score_cluster_indexes(
    return out;
 }
 
-RVec< double > Ecal_Analysis_Class::get_score_cluster_loc(RVec< std::vector<int> > indexes,
-                                                          RVec<double> mc_score_x, RVec<double> mc_score_pz){
+vector< double > Ecal_Analysis_Class::get_score_cluster_loc(vector< vector<int> > indexes,
+                                                          vector<double> mc_score_x, vector<double> mc_score_pz){
    // Returns the cluster location. Pass mc_score_x to get x or mc_score_y to get y.
    // The cluster location is computed as the pz weighted average of the hit locations.
-   RVec<double> out;
+   vector<double> out;
    for(size_t i=0; i<indexes.size(); ++i){
       double ave = 0;
       double sum = 0;
@@ -102,9 +102,9 @@ RVec< double > Ecal_Analysis_Class::get_score_cluster_loc(RVec< std::vector<int>
    return out;
 }
 
-RVec< double > Ecal_Analysis_Class::get_score_cluster_pz(RVec< std::vector<int> > indexes, RVec<double> mc_score_pz){
+vector< double > Ecal_Analysis_Class::get_score_cluster_pz(vector< vector<int> > indexes, vector<double> mc_score_pz){
    // Returns the summed pz (or py or px if passed those.)
-   RVec<double> out;
+   vector<double> out;
    for(size_t i=0; i<indexes.size(); ++i){
       double sum = 0;
       for(size_t j=0; j< indexes[i].size(); ++j){
@@ -115,10 +115,10 @@ RVec< double > Ecal_Analysis_Class::get_score_cluster_pz(RVec< std::vector<int> 
    return out;
 }
 
-RVec< double > Ecal_Analysis_Class::get_score_cluster_e(RVec< std::vector<int> > indexes,
-                                   RVec<double> mc_score_px, RVec<double> mc_score_py, RVec<double> mc_score_pz){
+vector< double > Ecal_Analysis_Class::get_score_cluster_e(vector< vector<int> > indexes,
+                                   vector<double> mc_score_px, vector<double> mc_score_py, vector<double> mc_score_pz){
    // Return the energy as Sum(sqrt(px**2 + py**2 + pz**2))
-   RVec<double> out;
+   vector<double> out;
    for(size_t i=0; i<indexes.size(); ++i){
       double sum = 0;
       for(size_t j=0; j< indexes[i].size(); ++j){
@@ -132,8 +132,8 @@ RVec< double > Ecal_Analysis_Class::get_score_cluster_e(RVec< std::vector<int> >
 
 
 
-RVec<bool> Ecal_Analysis_Class::fiducial_cut(RVec<int> ix, RVec<int> iy){ /// Fiducial cut for basic fiducial region.
-   RVec<bool> out;
+vector<bool> Ecal_Analysis_Class::fiducial_cut(vector<int> ix, vector<int> iy){ /// Fiducial cut for basic fiducial region.
+   vector<bool> out;
    for(size_t i=0;i< ix.size();++i){
       if(ficucial_cut_test(ix[i], iy[i]) )
       {
@@ -145,8 +145,8 @@ RVec<bool> Ecal_Analysis_Class::fiducial_cut(RVec<int> ix, RVec<int> iy){ /// Fi
    return out;
 }
 
-RVec<bool> Ecal_Analysis_Class::fiducial_cut_extended(RVec<int> ix, RVec<int> iy){
-   RVec<bool> out;
+vector<bool> Ecal_Analysis_Class::fiducial_cut_extended(vector<int> ix, vector<int> iy){
+   vector<bool> out;
    for(size_t i=0;i< ix.size();++i){
       if(ficucial_cut_test(ix[i], iy[i]))
       {
@@ -176,9 +176,9 @@ RVec<bool> Ecal_Analysis_Class::fiducial_cut_extended(RVec<int> ix, RVec<int> iy
    return out;
 }
 
-RVec<int> Ecal_Analysis_Class::get_score_n_primary_hits(RVec<double> &mc_part_z, RVec<int> &mc_score_part_idx,
-                                                        RVec<double> &mc_score_z, RVec<double> &mc_score_pz) const {
-   RVec<int> out;
+vector<int> Ecal_Analysis_Class::get_score_n_primary_hits(vector<double> &mc_part_z, vector<int> &mc_score_part_idx,
+                                                        vector<double> &mc_score_z, vector<double> &mc_score_pz) const {
+   vector<int> out;
    for(size_t i_part=0; i_part < mc_part_z.size(); ++i_part){
       if(abs(mc_part_z[i_part]) < 1e-6 ){
          int count = 0;
@@ -193,9 +193,9 @@ RVec<int> Ecal_Analysis_Class::get_score_n_primary_hits(RVec<double> &mc_part_z,
    return out;
 }
 
-RVec<int> Ecal_Analysis_Class::get_score_n_secondary_hits(RVec<double> &mc_part_z, RVec<int> &mc_score_part_idx,
-                                                          RVec<double> &mc_score_z, RVec<double> &mc_score_pz) const {
-   RVec<int> out;
+vector<int> Ecal_Analysis_Class::get_score_n_secondary_hits(vector<double> &mc_part_z, vector<int> &mc_score_part_idx,
+                                                          vector<double> &mc_score_z, vector<double> &mc_score_pz) const {
+   vector<int> out;
    for(size_t i_part=0; i_part < mc_part_z.size(); ++i_part){
       if(abs(mc_part_z[i_part]) >= 1e-6 ){
          int count = 0;
@@ -214,30 +214,30 @@ RVec<int> Ecal_Analysis_Class::get_score_n_secondary_hits(RVec<double> &mc_part_
    return out;
 }
 
-RVec<int> Ecal_Analysis_Class::get_list_of_primary_mc(RVec<double> &part_z) {
+vector<int> Ecal_Analysis_Class::get_list_of_primary_mc(vector<double> &part_z) {
    /// Get a list of indexes to MCParticles (mc_part_*) of the primary particles, i.e. from the generator.
    /// We do not need to check for proximity, since hits caused by primaries should always be close anyway.
-   RVec<int> out;
+   vector<int> out;
    for(size_t i=0; i< part_z.size(); ++i)
       if(abs(part_z[i]) < 1e-6) out.push_back(i);
    return out;
 }
 
-RVec<int> Ecal_Analysis_Class::get_list_of_all_secondary_mc(RVec<double> &part_z) {
+vector<int> Ecal_Analysis_Class::get_list_of_all_secondary_mc(vector<double> &part_z) {
    /// Get a list of all secondary MCParticles (mc_part_*), i.e. not primary ones.
-   RVec<int> out;
+   vector<int> out;
    for(size_t i=0; i< part_z.size(); ++i)
       if(abs(part_z[i]) >= 1e-6) out.push_back(i);
    return out;
 }
 
-RVec<int> Ecal_Analysis_Class::get_centers_of_scored_secondary_mc(RVec<double> &part_z, RVec<int> &mc_score_part_idx, RVec<double> &mc_score_x,
-                                                 RVec<double> &mc_score_y, RVec<double> &mc_score_z, RVec<double> &mc_score_pz) const{
+vector<int> Ecal_Analysis_Class::get_centers_of_scored_secondary_mc(vector<double> &part_z, vector<int> &mc_score_part_idx, vector<double> &mc_score_x,
+                                                 vector<double> &mc_score_y, vector<double> &mc_score_z, vector<double> &mc_score_pz) const{
    /// Returns a list of the indexes of the "seed" hits on the score plane, i.e. a list of the indexes of the highest
    /// momentum score hits caused by those secondary MCParticles (mc_part_*) that cross the ECal
    /// scoring plane in positive z direction, and are the highest pz within mc_score_close_cut distance in x and y.
-   RVec<int> out;
-   std::vector<bool> already_used(mc_score_z.size(), false);
+   vector<int> out;
+   vector<bool> already_used(mc_score_z.size(), false);
    bool not_done = true;
    while(not_done) {
       double max_val = 0;
@@ -269,10 +269,10 @@ RVec<int> Ecal_Analysis_Class::get_centers_of_scored_secondary_mc(RVec<double> &
 }
 
 
-RVec<double> Ecal_Analysis_Class::get_score_primary_hits_energy(RVec<double> &mc_part_z, RVec<int> &mc_score_part_idx,
-                                                                RVec<double> &mc_score_z, RVec<double> &mc_score_px,
-                                                                RVec<double> &mc_score_py, RVec<double> &mc_score_pz) {
-   RVec<double> out;
+vector<double> Ecal_Analysis_Class::get_score_primary_hits_energy(vector<double> &mc_part_z, vector<int> &mc_score_part_idx,
+                                                                vector<double> &mc_score_z, vector<double> &mc_score_px,
+                                                                vector<double> &mc_score_py, vector<double> &mc_score_pz) {
+   vector<double> out;
    for(size_t i_part=0; i_part < mc_part_z.size(); ++i_part){
       if(abs(mc_part_z[i_part]) < 1e-6 ) {
          double sum = 0;
@@ -289,10 +289,10 @@ RVec<double> Ecal_Analysis_Class::get_score_primary_hits_energy(RVec<double> &mc
    return out;
 }
 
-RVec<double> Ecal_Analysis_Class::get_score_secondary_hits_energy(RVec<double> &mc_part_z, RVec<int> &mc_score_part_idx,
-                                                                  RVec<double> &mc_score_z, RVec<double> &mc_score_px,
-                                                                  RVec<double> &mc_score_py, RVec<double> &mc_score_pz) {
-   RVec<double> out;
+vector<double> Ecal_Analysis_Class::get_score_secondary_hits_energy(vector<double> &mc_part_z, vector<int> &mc_score_part_idx,
+                                                                  vector<double> &mc_score_z, vector<double> &mc_score_px,
+                                                                  vector<double> &mc_score_py, vector<double> &mc_score_pz) {
+   vector<double> out;
    for(size_t i_part=0; i_part < mc_part_z.size(); ++i_part){
       if(abs(mc_part_z[i_part]) >= 1e-6) {
          double sum = 0;
@@ -312,7 +312,7 @@ RVec<double> Ecal_Analysis_Class::get_score_secondary_hits_energy(RVec<double> &
 double Ecal_Analysis_Class::ecal_xpos_to_index(double xpos) {
    // Return the (double!!!) value of the index  given the x position. This is used to plot on an ix, iy histogram.
    int offset = 0;
-   const std::vector<double> crystal_pos_x{-298.73819, -282.96774, -267.28214, -251.67653, -236.14636, -220.68710,
+   const vector<double> crystal_pos_x{-298.73819, -282.96774, -267.28214, -251.67653, -236.14636, -220.68710,
                                            -205.29440,
                                            -189.96397, -174.69170, -159.47348, -144.30539, -129.18352, -114.10406,
                                            -99.06329,
@@ -324,7 +324,7 @@ double Ecal_Analysis_Class::ecal_xpos_to_index(double xpos) {
                                            260.05212, 275.32440, 290.65482, 306.04755, 321.50677, 337.03696, 352.64255,
                                            368.32819, 384.09863};
 
-   auto const idx = std::lower_bound(crystal_pos_x.begin(), crystal_pos_x.end(), xpos);
+   auto const idx = lower_bound(crystal_pos_x.begin(), crystal_pos_x.end(), xpos);
    if (idx == crystal_pos_x.end()) { return -1.; }
 
    if (xpos > (crystal_pos_x[22] + crystal_pos_x[23]) / 2.) {
@@ -333,7 +333,7 @@ double Ecal_Analysis_Class::ecal_xpos_to_index(double xpos) {
       offset = -23;
    }
 
-   int i_idx = std::distance(crystal_pos_x.begin(), idx);
+   int i_idx = distance(crystal_pos_x.begin(), idx);
 // Interpolate between idx and idx+1.
    if (i_idx >= crystal_pos_x.size()) {
       i_idx = crystal_pos_x.size()-1;
--- MC2021/Ecal_Analysis_Class.h
+++ MC2021/Ecal_Analysis_Class.h
@@ -38,12 +38,14 @@
 ///
 
 #include <string>
+#include <vector>
+using namespace std;
 #include "TObject.h"
 #include "ROOT/RDataFrame.hxx"
 #include "ROOT/RDF/InterfaceUtils.hxx"
 #include "ROOT/RDF/RInterface.hxx"
 #include "ROOT/RDF/HistoModels.hxx"
-#include "ROOT/RVec.hxx"
+
 
 using namespace ROOT;
 using namespace ROOT::VecOps;
@@ -52,7 +54,7 @@ using RNode = ROOT::RDF::RNode;
 class Ecal_Analysis_Class {
 
 public:
-   std::vector< std::pair<int,int> > fiducial_cut_exclude;
+   vector< pair<int,int> > fiducial_cut_exclude;
    double mc_score_z_position_cut = 1400;
    double mc_score_pz_cut = 0.01;
    double mc_score_close_cut = 3*15.09; // About 3 crystal widths.
@@ -61,7 +63,7 @@ public:
    // crystal_factors = [0.06600114645748535,0.06600114778752586,0.0665927423975814,0.06659274408814787]
 
 public:
-   std::string Version(){ return "V1.0.3";}
+   string Version(){ return "V1.0.4";}
 
    // Note: I tried templating this, with instantiations to make the templates resolve. This works at the root prompt,
    // but in Python it could not resolve the correct template. Given that the RNode is a "wicked" complicated item, we just overload.
@@ -70,24 +72,24 @@ public:
    // it up. Instead for Python, call this method with: dfx = EAC.extend_dataframe(R.RDF.AsRNode(df))
    RNode extend_dateframe(RDataFrame in){ return extend_dataframe( (RNode)(in));}
 
-   RVec< std::vector<int> > get_score_cluster_indexes( RVec<double> mc_score_pz,
-         RVec<double> mc_score_x, RVec<double> mc_score_y, RVec<double> mc_score_z);
+   vector< vector<int> > get_score_cluster_indexes( vector<double> mc_score_pz,
+         vector<double> mc_score_x, vector<double> mc_score_y, vector<double> mc_score_z);
 
-   RVec< double > get_score_cluster_loc(RVec< std::vector<int> > indexes, RVec<double> mc_score_x, RVec<double> mc_score_pz);
-   RVec< double > get_score_cluster_pz(RVec< std::vector<int> > indexes, RVec<double> mc_score_pz);
-   RVec< double > get_score_cluster_e(RVec< std::vector<int> > indexes,
-                                      RVec<double> mc_score_px, RVec<double> mc_score_py, RVec<double> mc_score_pz);
+   vector< double > get_score_cluster_loc(vector< vector<int> > indexes, vector<double> mc_score_x, vector<double> mc_score_pz);
+   vector< double > get_score_cluster_pz(vector< vector<int> > indexes, vector<double> mc_score_pz);
+   vector< double > get_score_cluster_e(vector< vector<int> > indexes,
+                                      vector<double> mc_score_px, vector<double> mc_score_py, vector<double> mc_score_pz);
 
-   static RVec<int> get_list_of_primary_mc(RVec<double> &part_z);
-   static RVec<int> get_list_of_all_secondary_mc(RVec<double> &part_z);
-   RVec<int> get_centers_of_scored_secondary_mc(RVec<double> &part_z, RVec<int> &mc_score_part_idx, RVec<double> &mc_score_x,
-                                                    RVec<double> &mc_score_y, RVec<double> &mc_score_z, RVec<double> &mc_score_pz) const;
-   RVec<double> get_score_primary_hits_energy(RVec<double> &mc_part_z, RVec<int> &mc_score_part_idx, RVec<double> &mc_score_z,
-                                              RVec<double> &mc_score_px, RVec<double> &mc_score_py, RVec<double> &mc_score_pz);
-   RVec<double> get_score_secondary_hits_energy(RVec<double> &mc_part_z, RVec<int> &mc_score_part_idx, RVec<double> &mc_score_z,
-                                              RVec<double> &mc_score_px, RVec<double> &mc_score_py, RVec<double> &mc_score_pz);
-   RVec<int> get_score_n_primary_hits(RVec<double> &mc_part_z, RVec<int> &mc_score_part_idx, RVec<double> &mc_score_z, RVec<double> &mc_score_pz) const;
-   RVec<int> get_score_n_secondary_hits(RVec<double> &mc_part_z, RVec<int> &mc_score_part_idx, RVec<double> &mc_score_z, RVec<double> &mc_score_pz) const;
+   static vector<int> get_list_of_primary_mc(vector<double> &part_z);
+   static vector<int> get_list_of_all_secondary_mc(vector<double> &part_z);
+   vector<int> get_centers_of_scored_secondary_mc(vector<double> &part_z, vector<int> &mc_score_part_idx, vector<double> &mc_score_x,
+                                                    vector<double> &mc_score_y, vector<double> &mc_score_z, vector<double> &mc_score_pz) const;
+   vector<double> get_score_primary_hits_energy(vector<double> &mc_part_z, vector<int> &mc_score_part_idx, vector<double> &mc_score_z,
+                                              vector<double> &mc_score_px, vector<double> &mc_score_py, vector<double> &mc_score_pz);
+   vector<double> get_score_secondary_hits_energy(vector<double> &mc_part_z, vector<int> &mc_score_part_idx, vector<double> &mc_score_z,
+                                              vector<double> &mc_score_px, vector<double> &mc_score_py, vector<double> &mc_score_pz);
+   vector<int> get_score_n_primary_hits(vector<double> &mc_part_z, vector<int> &mc_score_part_idx, vector<double> &mc_score_z, vector<double> &mc_score_pz) const;
+   vector<int> get_score_n_secondary_hits(vector<double> &mc_part_z, vector<int> &mc_score_part_idx, vector<double> &mc_score_z, vector<double> &mc_score_pz) const;
    inline static bool ficucial_cut_test(int ix, int iy){  /// Test if ix and iy are in basic fiducial region.
       return !(ix <= -23 || ix >= 23) && /* Cut out the left and right side */
              !(iy <= -6 || iy >= 6)   && /* Cut out the top and bottom row */
@@ -99,8 +101,8 @@ public:
       fiducial_cut_exclude.push_back( {ix, iy});
    }
 
-   static RVec<bool> fiducial_cut(RVec<int> ix, RVec<int> iy); /// Fiducial cut for basic fiducial region.
-   RVec<bool> fiducial_cut_extended(RVec<int> ix, RVec<int> iy);  /// Fiducial cut extended with bad crystals from list.
+   static vector<bool> fiducial_cut(vector<int> ix, vector<int> iy); /// Fiducial cut for basic fiducial region.
+   vector<bool> fiducial_cut_extended(vector<int> ix, vector<int> iy);  /// Fiducial cut extended with bad crystals from list.
 
    static double ecal_xpos_to_index(double xpos);
    static double ecal_ypos_to_index(double ypos);
